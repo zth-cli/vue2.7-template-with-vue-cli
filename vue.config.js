aff9c0ac44d5a2749abf6fe8844a5fe97e927f54
rzx100 <770304867@qq.com>
2021-10-08 14:45:40 +0800

    展示优化

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/': { // search为转发路径
+      '/': {
         target: 'http://127.0.0.1:8090', // 目标地址
         ws: true, // 是否代理websockets
         changeOrigin: true // 设置同源 默认false，是否需要改变原始主机头为目标URL,
